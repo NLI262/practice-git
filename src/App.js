f3c704e833b80734fecf94480586e957c9eb84c9
NLI262 <sri.harsha@nineleaps.com>
2020-06-04 14:19:17 +0530

    Header text

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './Button';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     <Button text="me"/>
     </div>
   );
